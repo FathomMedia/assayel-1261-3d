75ffee055fc6529665dd86a30ffe109d647876dd
Mohamed Mukhtar <mukxdev@gmail.com>
2023-07-26 10:41:43 +0300

    added draggability and scroll to selected

--- components/ListOfBuildings.tsx
+++ components/ListOfBuildings.tsx
@@ -3,6 +3,7 @@ import React, { FC, useEffect, useRef } from "react";
 import { Button } from "@/components/ui/button";
 import { LuChevronLeft, LuChevronRight } from "react-icons/lu";
 import { cn } from "@/src/utils";
+import { useDraggable } from "react-use-draggable-scroll";
 
 interface Props {
   buildings: Building[];
@@ -21,6 +22,11 @@ export const ListOfBuildings: FC<Props> = ({
   const canGoPrevious = currentIndex > 0 && currentIndex !== -1;
   const canGoNext = currentIndex < buildings.length - 1 && buildings.length > 0;
   const itemsRef = useRef<(HTMLButtonElement | null)[]>([]);
+
+  const ref =
+    useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>;
+  const { events } = useDraggable(ref);
+
   function goPrevious() {
     if (canGoPrevious) {
       onClickBuilding && onClickBuilding(buildings[currentIndex - 1]);
@@ -35,13 +41,14 @@ export const ListOfBuildings: FC<Props> = ({
   function goTo(building: Building) {
     onClickBuilding && onClickBuilding(building);
   }
-
   useEffect(() => {
-    //? this is removed cuz it scrolls the wordpress website to the name as well as the iframe
-    // itemsRef.current[currentIndex]?.scrollIntoView({ behavior: "smooth" });
+    itemsRef.current[currentIndex]?.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+    });
 
     return () => {};
-  }, []);
+  }, [currentIndex]);
 
   return (
     <div
@@ -58,13 +65,17 @@ export const ListOfBuildings: FC<Props> = ({
       >
         <LuChevronLeft className="w-6 h-6 text-foreground" />
       </Button>
-      <div className="flex w-full py-2 overflow-x-scroll sm:py-3 grow">
+      <div
+        ref={ref}
+        {...events}
+        className="flex w-full py-2 overflow-x-scroll no-scrollbar sm:py-3 grow"
+      >
         <div className="flex duration-300 grow animate-in slide-in-from-left-20">
           <div className="flex w-full gap-3">
             {...buildings.map((b, i) => (
               <Button
                 variant={selected?.id === b.id ? "default" : "secondary"}
-                className={`text-xs font-normal sm:text-base px-3 w-fit whitespace-nowrap hover:bg-primary hover:text-primary-foreground ${
+                className={`text-xs flex-none font-normal sm:text-base px-3 w-fit whitespace-nowrap hover:bg-primary hover:text-primary-foreground ${
                   selected?.id !== b.id && "bg-background text-foreground"
                 }`}
                 ref={(element) => (itemsRef.current[i] = element)}
