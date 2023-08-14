b78c4202e5db0b72bb68c61f9e5fd2e418c76058
Mohamed Mukhtar <mukxdev@gmail.com>
2023-08-14 17:18:44 +0300

    removed persistSession from Supabase

--- src/database/supabase.ts
+++ src/database/supabase.ts
@@ -1,5 +1,12 @@
-import { createClient } from "@supabase/supabase-js";
+import { SupabaseClientOptions, createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_API ?? "";
-export const supabase = createClient(supabaseUrl, supabaseKey);
+
+const options: SupabaseClientOptions<"public"> = {
+  auth: {
+    persistSession: false,
+  },
+};
+
+export const supabase = createClient(supabaseUrl, supabaseKey, options);
